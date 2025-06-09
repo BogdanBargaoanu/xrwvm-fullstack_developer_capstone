aadccbaed20f18afa9c019259ad2eaef2c3fb0eb
BogdanBargaoanu <bn19bba@yahoo.com>
2025-06-09 16:23:51 +0300

    fix 2

--- server/database/app.js
+++ server/database/app.js
@@ -2,21 +2,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const fs = require('fs');
-const cors = require('cors')
-const app = express()
+const cors = require('cors');
+const app = express();
 const port = 3030;
 
-app.use(cors())
+app.use(cors());
 app.use(require('body-parser').urlencoded({ extended: false }));
 
 const reviews_data = JSON.parse(fs.readFileSync("reviews.json", 'utf8'));
 const dealerships_data = JSON.parse(fs.readFileSync("dealerships.json", 'utf8'));
 
-mongoose.connect("mongodb://mongo_db:27017/", { 'dbName': 'dealershipsDB' });
-
+mongoose.connect("mongodb://mongo_db:27017/", { dbName: 'dealershipsDB' });
 
 const Reviews = require('./review');
-
 const Dealerships = require('./dealership');
 
 try {
@@ -26,7 +24,6 @@ try {
   Dealerships.deleteMany({}).then(() => {
     Dealerships.insertMany(dealerships_data.dealerships);
   });
-
 } catch (error) {
   res.status(500).json({ error: 'Error fetching documents' });
 }
